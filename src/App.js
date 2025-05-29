ac8b59868487bec6312c0cdef3fc0376521ee24e
Tawfiq Akroot <tawfiq.ux3@gmail.com>
2025-05-29 11:48:42 +0300

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
-import React from 'react';
+
 import { Outlet } from 'react-router-dom';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the App</h1>
-      {/* هذا يعرض المكونات الفرعية حسب الراوتر */}
+       <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography> */}
+          <Button sx={{display:"flex",justifyContent:"flex-end",marginLeft:"auto"}} color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
       <Outlet />
     </div>
   );
