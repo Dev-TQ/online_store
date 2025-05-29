f256d5c0234015e5e7d1c5a80379a5a6ff9d642a
Tawfiq Akroot <tawfiq.ux3@gmail.com>
2025-05-29 12:56:15 +0300

    Typography

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './index.css'; 
 import ReactDOM from 'react-dom/client';
 import Router from './page/Rooter/Rout'; // مسار ملف الراوتر اللي انت عملته
 import { RouterProvider } from 'react-router-dom';
-
+import { createTheme } from '@mui/material';
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/page/HOME/Home.jsx
+++ src/page/HOME/Home.jsx
@@ -1,11 +1,17 @@
 import { createRoutesFromChildren } from "react-router-dom"
 import "./Home.css"
 import React from 'react';
+import { Typography } from "@mui/material";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+    <Typography noWrap  sx={{marginTop:"auto"}} color="primary" variant="h4" component="div">
+      Welcome to the Home pageWelcome to the Home pageWelcome to the Home pageWelcome to the Home pageWelcome to the Home pageWelcome to the Home page
+    </Typography>
+    <Typography gutterBottom variant="body1" color="initial">ddddd</Typography>
+    <Typography  variant="body1" color="initial">ddddd</Typography>
+    <Typography sx={{margin:"100px"}}  variant="inherit" color="initial">this typographt</Typography>
     </div>
   );
 }
