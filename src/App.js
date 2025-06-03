932202910a19f81f7c52c714785c593126c9f7ad
Tawfiq Akroot <tawfiq.ux3@gmail.com>
2025-06-03 21:58:35 +0300

    Avatar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 
-
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet,Link as RouterLink  } from 'react-router-dom';
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import  {AppBar,Box,Typography,Button,IconButton,Toolbar,Avatar, Link as MuiLink }from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import theme from 'Theme/Theme';
+import { useState } from 'react';
 
 const App = () => {
+  const [name,setname]=useState("salman")
   return (
-    <div>
-       <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+       <Box >
+      <AppBar position="static" color='primary'>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography> */}
-          <Button sx={{display:"flex",justifyContent:"flex-end",marginLeft:"auto"}} color="inherit">Login</Button>
+     <MuiLink component={RouterLink} to="/Create" underline='none' sx={{ color:"black",background:"none",flexGrow:1,display:"flex"}}>
+          <MenuIcon />
+      </MuiLink>
+     <MuiLink component={RouterLink} to="/" underline='none' sx={{ color:"black",background:"none"}}>
+        Towfiqe
+      </MuiLink>
+      <MuiLink component={RouterLink} to="/Home" sx={{background:"none", color:"black"}}>
+      <Avatar src='./image/man.jpg' alt="Shape">{name.charAt(0)}</Avatar>
+      </MuiLink>
         </Toolbar>
       </AppBar>
-    </Box>
       <Outlet />
-    </div>
+    </Box>
+
   );
 }
 
